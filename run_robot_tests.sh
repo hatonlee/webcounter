cc131ef7b758f74f9e4b350e9e890eea6fdb5727
hatonlee <hatonen.leevi@gmail.com>
2025-11-12 15:19:24 +0200

    fix: changed port to 5001

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -8,7 +8,7 @@ poetry run python3 src/index.py &
 echo "started Flask server"
 
 # odotetaan, että palvelin on valmiina ottamaan vastaan pyyntöjä
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000)" != "200" ]];
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5001)" != "200" ]];
   do sleep 1;
 done
 
@@ -19,7 +19,7 @@ poetry run robot --variable HEADLESS:true src/tests
 
 status=$?
 
-# pysäytetään Flask-palvelin portissa 5000
-kill $(lsof -t -i:5000)
+# pysäytetään Flask-palvelin portissa 5001
+kill $(lsof -t -i:5001)
 
 exit $status
